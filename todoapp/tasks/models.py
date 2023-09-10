e36678656dea50568e9e9450b350817739aa5ba5
Ashish Sharda <ashishjsharda@gmail.com>
2023-09-10 16:13:34 -0500

    add views and forms

--- todoapp/tasks/models.py
+++ todoapp/tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date = models.DateTimeField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
